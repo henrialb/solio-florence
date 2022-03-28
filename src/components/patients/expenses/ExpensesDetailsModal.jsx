4740bf625e3a726b35238e43f9236662c2e102ed
Ines Louro <inespazlouro@gmail.com>
2022-03-28 21:44:21 +0100

    edit/view logic in modal

--- src/components/patients/expenses/ExpensesDetailsModal.jsx
+++ src/components/patients/expenses/ExpensesDetailsModal.jsx
@@ -9,6 +9,11 @@ import {
   CModalTitle,
   CModalBody,
   CModalFooter,
+  CForm,
+  CCol,
+  CFormLabel,
+  CFormInput,
+  CFormTextarea,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilNotes } from '@coreui/icons'
@@ -17,6 +22,7 @@ import PropTypes from 'prop-types'
 
 const ExpensesDetailsModal = ({ expense }) => {
   const [visible, setVisible] = useState(false)
+  const [editMode, setEditMode] = useState(false)
 
   if (expense.length === 0) {
     return null
@@ -40,21 +46,64 @@ const ExpensesDetailsModal = ({ expense }) => {
           )}
         </CTableDataCell>
       </CTableRow>
-      <CModal alignment="center" visible={visible} onClose={() => setVisible(false)}>
+      <CModal
+        alignment="center"
+        visible={visible}
+        onClose={() => {
+          setVisible(false)
+          setEditMode(false)
+        }}
+      >
         <CModalHeader>
-          <CModalTitle>O Título da despesa</CModalTitle>
+          <CModalTitle>Despesa</CModalTitle>
         </CModalHeader>
         <CModalBody>
-          Data: {expense.date} <br></br>
-          Descrição: {expense.description} <br></br>
-          Valor: {expense.amount} <br></br>
-          Nota: {expense.note}
+          <CForm className="row g-3">
+            <CCol md={12}>
+              <CFormLabel htmlFor="inputDescription">Descrição</CFormLabel>
+              <CFormInput
+                id="inputDescription"
+                defaultValue={expense.description}
+                disabled={!editMode}
+              />
+            </CCol>
+            <CCol md={6}>
+              <CFormLabel htmlFor="inputDate">Data</CFormLabel>
+              <CFormInput id="inputDate" defaultValue={expense.date} disabled={!editMode} />
+            </CCol>
+            <CCol md={6}>
+              <CFormLabel htmlFor="inputAmount">Valor</CFormLabel>
+              <CFormInput
+                type="decimal"
+                id="inputAmount"
+                defaultValue={expense.amount}
+                disabled={!editMode}
+              />
+            </CCol>
+            <CCol md={12}>
+              <CFormLabel htmlFor="inputNote">Nota</CFormLabel>
+              <CFormTextarea id="inputNote" defaultValue={expense.note} disabled={!editMode} />
+            </CCol>
+          </CForm>
         </CModalBody>
-        <CModalFooter>
-          <CButton color="secondary" onClick={() => setVisible(false)}>
+        <CModalFooter className="d-flex justify-content-between">
+          <CButton
+            color="secondary"
+            size="sm"
+            className="text-white"
+            onClick={() => setVisible(false)}
+          >
             Fechar
           </CButton>
-          <CButton color="primary">Editar despesa</CButton>
+          {!editMode ? (
+            <CButton color="primary" size="sm" onClick={() => setEditMode(!editMode)}>
+              Editar despesa
+            </CButton>
+          ) : (
+            <CButton color="primary" size="sm" onClick={() => setEditMode(!editMode)}>
+              Guardar
+            </CButton>
+          )}
         </CModalFooter>
       </CModal>
     </>
